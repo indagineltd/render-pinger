bd5de943f537d7fb82a192875427f6292481d702
Indagine LTD <indagineltd@gmail.com>
2025-10-08 15:13:20 +0200

    Update index.js
    adjustment to the index.js

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const https = require('https');
 
-const url = 'https://www.tuttowebdev.com/'
+const urls = [
+  'https://www.tuttowebdev.com/',
+  'https://www.tuttowebdev.com/about',
+  'https://www.tuttowebdev.com/amico',
+  'https://www.tuttowebdev.com/scrivo',
+  'https://www.tuttowebdev.com/webdev',
+  'https://www.tuttowebdev.com/webdev#fullstack',
+  'https://www.tuttowebdev.com/webdev#seo',
+  'https://www.tuttowebdev.com/webdev#frontend',
+  'https://www.tuttowebdev.com/webdev#backend',
+  'https://www.tuttowebdev.com/contact'
+];
 
 function pingSite() {
+  const url = urls[Math.floor(Math.random() * urls.length)];
   https.get(url, (res) => {
-    console.log(`Pinged ${url} - Status: ${res.statusCode}`)
+    console.log(`[${new Date().toLocaleTimeString()}] Pinged ${url} - Status: ${res.statusCode}`);
   }).on('error', (err) => {
-    console.error(`Error in Pinging ${url}:`, err.message)
+    console.error(`[${new Date().toLocaleTimeString()}] Error in Pinging ${url}:`, err.message);
   });
-};
+}
 
-// Ping every 1 minute
-setInterval(pingSite, 1 * 60 * 1000);
-
-// Call the function
-pingSite();
+// Ping every 12 minutes
+setInterval(pingSite, 12 * 60 * 1000);
+pingSite(); // Initial ping
